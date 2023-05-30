10e5ef88f1ebb8a2a3dd6d646e7c52be45c3a4e0
ivanov.io <iliya00817@gmail.com>
2023-05-30 11:45:48 +0300

    Spring without boot update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,28 @@
 package org.example;
 
 
+import org.example.beans.AnnotationBean;
+import org.example.beans.JavaConfigBean;
+import org.example.beans.JavaConfigBeanTestClass;
+import org.example.beans.TestXmlBeanClass;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
+        var xmlApplicationContext = new ClassPathXmlApplicationContext("config.xml");
+        TestXmlBeanClass xmlBeanClass = xmlApplicationContext.getBean(TestXmlBeanClass.class);
+        xmlBeanClass.displayInfo();
+        var annotationConfigApplicationContext = new AnnotationConfigApplicationContext(JavaConfigBean.class);
+        var innerConfigBean = annotationConfigApplicationContext.getBean(JavaConfigBeanTestClass.class);
+        innerConfigBean.displayInfo();
+        var annotationBean = annotationConfigApplicationContext.getBean(AnnotationBean.class);
+        annotationBean.displayInfo();
+
     }
+
 }
